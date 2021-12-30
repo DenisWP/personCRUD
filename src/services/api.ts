51d7f02821030b4fca262533312d2c46818314e5
bi004940 <denis.dabliu@gmail.com>
2021-12-30 10:38:12 -0300

    Inicio da Comunicacao API Natali

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,9 +2,12 @@ import axios from "axios";
 
 const api = axios.create({
     baseURL: "https://crudpessoaqa.herokuapp.com/",
-    headers: {
-        "Content-type": "application/json"
-    }
+    /*headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Authorization",
+        "Access-Control-Allow-Methods": "GET, POST, OPTIONS, PUT, PATCH, DELETE" ,
+        "Content-Type": "application/json;charset=UTF-8"
+    }*/
 })
 
 export default api;
